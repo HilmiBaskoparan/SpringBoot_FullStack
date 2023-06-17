5e752a980fa280f4e0a49ebe1e73152205bbd172
Hilmi Baskoparan <h.baskoparan@gmail.com>
2023-06-17 17:03:17 +0300

    Header.jsx and other .jsx files. Close #17

---
+++ frontend/src/component/Header.jsx
@@ -0,0 +1,34 @@
+// rcc
+import React, { Component } from 'react'
+
+// CLASS COMPONENT
+export default class Header extends Component {
+    constructor(props){
+        super(props);
+
+        // bind
+
+        //state
+        this.state = {}
+
+        //bind
+    }
+
+    // CDM
+    componentDidMount() {  }
+
+    // FUNCTION
+
+    // RENDER
+    render() {
+
+        // RETURN
+        return (
+            // <div>Header</div>
+            // <React.Header>Header</React.Header>
+            <>
+                Header
+            </>
+        )
+    } // end render
+} // end class
