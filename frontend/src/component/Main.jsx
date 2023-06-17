d2b9af7f4112062b5f79f302734229a3b5a377d3
Hilmi Baskoparan <h.baskoparan@gmail.com>
2023-06-17 18:01:58 +0300

    Footer. Close #20

--- frontend/src/component/Main.jsx
+++ frontend/src/component/Main.jsx
@@ -27,7 +27,16 @@ export default class Main extends Component {
             // <div>Main</div>
             // <React.Main>Main</React.Main>
             <>
-                Main
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci porro vitae vel qui, hic repellat iusto nemo dolor. Excepturi vero eveniet adipisci laudantium amet temporibus possimus dolores exercitationem alias et?
+                Doloribus non ipsam sint inventore adipisci excepturi, accusantium nihil nesciunt soluta. Ad, impedit ut nihil ipsum accusamus, nisi maxime tenetur, iure quos labore et tempore. Deleniti iure laborum soluta error?
+                Perferendis libero veniam mollitia alias, neque nesciunt culpa corrupti magni rerum aliquid itaque officiis doloribus, accusantium facilis quaerat! Nisi ullam repellendus, nemo vel suscipit harum explicabo neque! Quia, molestias ex.
+                Velit numquam culpa natus reprehenderit nisi odit perferendis? Porro quibusdam, repellendus provident illum aspernatur nihil? Enim minus, dolorum, hic minima impedit cupiditate amet, perferendis delectus quia fugit molestiae modi facilis?
+                Alias quod reprehenderit, eveniet soluta quaerat sed iure assumenda dolorem totam nam quidem nihil in omnis vitae placeat ex eos, reiciendis autem perferendis rerum aut. Sed, facilis fugit! Ipsum, quaerat.
+                Animi suscipit aliquid cum perferendis nostrum, quidem beatae eaque cupiditate veniam totam quam tempora facere, voluptate corporis aperiam molestiae aut unde iusto. Obcaecati praesentium laudantium possimus dolore beatae delectus incidunt.
+                Quisquam nesciunt nemo adipisci obcaecati, ab optio tenetur quae praesentium odit! Omnis velit perferendis, labore sint fuga amet voluptas, voluptatem eos ratione qui alias error odit veritatis deserunt laborum necessitatibus.
+                Architecto ad doloremque consequatur consequuntur laborum ducimus fuga nulla excepturi. Pariatur vel adipisci delectus sit repellat explicabo atque quod. Dignissimos eius quas beatae dolore dolores tempora distinctio aliquid incidunt voluptatibus!
+                Dicta voluptatum aliquam ducimus, non officiis culpa! Earum doloribus doloremque soluta. Culpa, qui? Nobis beatae excepturi ea cum accusantium in maxime, voluptate temporibus ipsum tempore quam cupiditate mollitia dolores facilis!
+                Reprehenderit eaque quo dolore neque? Minima deserunt minus dolore? Et quis repellat quos suscipit saepe! Voluptatum ut inventore voluptatem dolores! Maiores ea commodi non accusantium corporis consequuntur ipsa expedita magnam!</p>
             </>
         )
     } // end render
