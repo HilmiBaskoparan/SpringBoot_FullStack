0f7eb26841cdf760b8b6b4d1829c813432be6b22
Hilmi Baskoparan <h.baskoparan@gmail.com>
2023-07-18 12:21:25 +0300

    RouterMain

--- frontend/src/component/RouterMain.jsx
+++ frontend/src/component/RouterMain.jsx
@@ -1,22 +1,38 @@
 // rcc
 import React, { Component } from 'react'
-import Footer from './Footer';
+
+// Header, Main,Footer
 import Header from './Header';
 import Main from './Main';
+import Footer from './Footer';
 
 // i18n 
 import { withTranslation } from 'react-i18next';
 
-// CLASS COMPONENT
+// HashRouter => Production için kullanın
+// HashRouter:Backtentten gidecek veya geolecek trafiği kapatıyorum kendimi güvence altına alıyorum
+// HashRouter as Router URL=>(http://localhost:3000/#/)
+// BrowserRouter => Test için
+// BrowserRouter: backent içiin network'u bütün detayına kadar bana göster
+// BrowserRouter as Router URL=>(http://localhost:3000/)
+import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom"
+
+// Blog
+import BlogList from './blog/BlogList';
+import BlogCreate from './blog/BlogCreate';
+import BlogUpdate from './blog/BlogUpdate';
+import BlogView from './blog/BlogView';
+
+// CLASS Component
 class RouterMain extends Component {
 
     // Componentteki yeni isim
-   static displayName="Blog_Router"
+    static displayName = "Blog_Router"
 
-    constructor(props){
+    constructor(props) {
         super(props);
 
-        // bind
+        //bind
 
         //state
         this.state = {}
@@ -25,26 +41,39 @@ class RouterMain extends Component {
     }
 
     // CDM
-    componentDidMount() {  }
+    componentDidMount() { }
 
     // FUNCTION
 
-    // RENDER
+    //RENDER
     render() {
 
-        // RETURN
+        //RETURN
         return (
-            // <div>RouterMain</div>
-            // <React.Fragment>FooRouterMainter</React.Fragment>
-            <>
-                <Header/>
-                <br />
-                <Main/>
-                <br />
-                <Footer/>
-            </>
-        )
-    } // end render
-} // end class
+            // <div>Header</div>
+            //<React.Fragment>Header</React.Fragment>
+            <React.Fragment>
+                <Router>
+                    <Header logo="fa-solid fa-blog" homePageLink="http://localhost:3000/index" />
+                    <div className="container">
+                        <Switch>
+                            <Route path="/" exact component={Main}></Route>
+                            <Route path="/index" component={Main}></Route>
 
-export default withTranslation()(RouterMain)
+                            <Route path="/blog/list" component={BlogList}></Route>
+                            <Route path="/blog/create" component={BlogCreate}></Route>
+                            <Route path="/blog/update/:id" component={BlogUpdate}></Route>
+                            <Route path="/blog/view/:id" component={BlogView}></Route>
+                            {/* eğer kullanıcı bad request atarsa(400) ana sayfaya yönlendirsin */}
+                            <Redirect to="/" />
+                        </Switch>
+                    </div>
+                    <Footer copy="&copy; Bütün haklar Saklıdır." />
+                </Router>
+
+            </React.Fragment>
+        ) //end return
+    } // end render
+}//end clas
+
+export default withTranslation()(RouterMain) 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+
 // RouterMain
 import RouterMain from './component/RouterMain';
+
 // CSS
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 
 // 18n (dil)
 import './internationalization/i18nlanguage'
 
 // DOM ROOT
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 // RENDER
 root.render(
   <React.StrictMode>
     <React.Fragment>
-      <RouterMain />
+      <RouterMain/>
     </React.Fragment>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
